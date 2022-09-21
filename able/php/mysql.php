eceaf26e1d42a7a58a4c4bdddb844092b16b2906
mien <2502mien@gmail.com>
2022-09-21 17:53:22 +0700

    add

--- able/php/mysql.php
+++ able/php/mysql.php
@@ -1,10 +1,11 @@
 <?php
-	$con = mysqli_connect("","","","");
+	$con = mysqli_connect("localhost", "root","","ktl");
 	if(!$con)
 	{
 		echo "error";
+	}else {
+		echo "done";
 	}
 //	@mysqli_select_db("todito", $con) or die("DB 연결실패");
-	mysqli_query("set names utf8");
 
 ?>
